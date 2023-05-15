bd3f37a0d3957a853cad7c313517e935dfe41c0c
Søren Øxenhave <sox@timelog.dk>
2023-05-15 14:02:13 +0200

    feat: add azure devops fetch

---
+++ Oexenhave.AzureDevOpsFetch/Oexenhave.AzureDevOpsFetch/Program.cs
@@ -0,0 +1,99 @@
+﻿using System.Text;
+using Microsoft.TeamFoundation.Core.WebApi;
+using Microsoft.TeamFoundation.SourceControl.WebApi;
+using Microsoft.VisualStudio.Services.Common;
+using Microsoft.VisualStudio.Services.WebApi;
+using Oexenhave.AzureDevOpsFetch;
+using ServiceStack.Text;
+
+if (args.Length != 2)
+{
+    Console.WriteLine("Please provide tenant URL and PAT as parameters. E.g. AzureDevOpsFetch.exe https://dev.azure.com/[tenantId] pgkxwdc7ikjm3jdvbiag6r7ylmbyxpahkty5ifr4mhls4tfpp7aa");
+    Console.WriteLine("Example:");
+    Console.WriteLine("   Oexenhave.AzureDevOpsFetch.exe https://dev.azure.com/[tenantId] pgkxwdc7ikjm3jdvbiag6r7ylmbyxpahkty5ifr4mhls4tfpp7aa");
+    Console.WriteLine("");
+    Console.WriteLine("Generate a PAT from https://dev.azure6.com/{tenant}/_usersSettings/tokens");
+    Console.WriteLine("Add access for \"Code (Read)\"");
+}
+else
+{
+    var collectionUri = args[0];
+    var pat = args[1];
+
+    // Connect to Azure DevOps Services
+    var credentials = new VssBasicCredential(string.Empty, pat);
+    var connection = new VssConnection(new Uri(collectionUri), credentials);
+
+    // Get a GitHttpClient to talk to the Git endpoints
+    using var gitClient = connection.GetClient<GitHttpClient>();
+    var projectClient = connection.GetClient<ProjectHttpClient>();
+    var projects = await projectClient.GetProjects();
+
+    var pullRequestStats = new List<PullRequestStat>();
+
+    foreach (var project in projects)
+    {
+        var projectRepos = await gitClient.GetRepositoriesAsync(project.Id);
+        foreach (var repository in projectRepos)
+        {
+            int skip = 0;
+            var pullRequests = await gitClient.GetPullRequestsAsync(project.Id, repository.Id,
+                new GitPullRequestSearchCriteria { Status = PullRequestStatus.All });
+
+            while (skip == 0 || pullRequestStats.Count > 0)
+            {
+                Console.WriteLine("[{0:O}] Processing {1}... ({2}/{3})", DateTime.Now, skip, project.Name,
+                    repository.Name);
+
+                if (pullRequests.Count == 0)
+                {
+                    break;
+                }
+
+                foreach (var pullRequest in pullRequests.Where(request =>
+                             request.IsDraft == false && request.Status != PullRequestStatus.Abandoned))
+                {
+                    var pullRequestStat = new PullRequestStat
+                    {
+                        ProjectName = project.Name,
+                        RepositoryName = repository.Name,
+                        PullRequestId = pullRequest.PullRequestId,
+                        Title = pullRequest.Title,
+                        CreatedAt = pullRequest.CreationDate,
+                        ClosedAt = pullRequest.ClosedDate,
+                        Status = pullRequest.Status,
+                        Reviewers = pullRequest.Reviewers,
+                        Creator = pullRequest.CreatedBy.DisplayName
+                    };
+
+                    var threads =
+                        await gitClient.GetThreadsAsync(project.Name, repository.Id, pullRequest.PullRequestId);
+                    var reviewThreads =
+                        threads.Where(thread =>
+                                thread.Properties.ContainsKey("CodeReviewThreadType") &&
+                                thread.Properties.ContainsValue("VoteUpdate"))
+                            .OrderByDescending(thread => thread.PublishedDate).ToList();
+
+                    var codeReviewedAt = reviewThreads.FirstOrDefault(thread =>
+                        thread.Comments.First().Author.DisplayName == pullRequestStat.CodeReviewer)?.PublishedDate;
+                    var qualityReviewedAt = reviewThreads.FirstOrDefault(thread =>
+                        thread.Comments.First().Author.DisplayName == pullRequestStat.QualityReviewer)?.PublishedDate;
+
+                    pullRequestStat.CodeReviewedAt = codeReviewedAt;
+                    pullRequestStat.QualityReviewedAt = qualityReviewedAt;
+
+                    pullRequestStats.Add(pullRequestStat);
+                }
+
+                skip += pullRequests.Count;
+                pullRequests = await gitClient.GetPullRequestsAsync(project.Id, repository.Id,
+                    new GitPullRequestSearchCriteria { Status = PullRequestStatus.All }, skip: skip);
+            }
+        }
+    }
+
+    File.WriteAllText("output.csv", CsvSerializer.SerializeToCsv(pullRequestStats), Encoding.UTF8);
+}
+
+Console.WriteLine("Press any key to close...");
+Console.ReadKey();
---
+++ Oexenhave.AzureDevOpsFetch/Oexenhave.AzureDevOpsFetch/PullRequestStat.cs
@@ -0,0 +1,107 @@
+﻿using System.Runtime.Serialization;
+using Microsoft.TeamFoundation.SourceControl.WebApi;
+
+namespace Oexenhave.AzureDevOpsFetch;
+
+public record PullRequestStat
+{
+    public string? ProjectName { get; set; }
+    
+    public string? RepositoryName { get; set; }
+    
+    public int PullRequestId { get; set; }
+    
+    [IgnoreDataMember]
+    public string? Title { get; set; }
+    
+    public DateTime CreatedAt { get; set; }
+    
+    public DateTime ClosedAt { get; set; }
+    
+    public PullRequestStatus Status { get; set; }
+
+    public double DaysToComplete => (ClosedAt == DateTime.MinValue ? DateTime.Now : ClosedAt).Subtract(CreatedAt).TotalDays;
+
+    public double DaysToCodeReview
+    {
+        get
+        {
+            var days = (CodeReviewedAt ?? DateTime.Now).Subtract(CreatedAt).TotalDays;
+
+            // Some PRs have no code approver - and it makes no sense to have it higher that the complete date
+            if (days > DaysToComplete)
+            {
+                return DaysToComplete;
+            }
+
+            return days;
+        }   
+    }
+    
+    public double DaysToQualityReview => (QualityReviewedAt ?? DateTime.Now).Subtract(CreatedAt).TotalDays;
+
+    public string StatusName
+    {
+        get
+        {
+            if (Status == PullRequestStatus.Completed)
+            {
+                return "Completed";
+            }
+
+            if (CodeReviewedAt == null && QualityReviewedAt == null)
+            {
+                return "AwaitCodeReview";
+            }
+            
+            if (CodeReviewedAt != null && QualityReviewedAt == null)
+            {
+                return "AwaitQualityReview";
+            }
+            
+            if (CodeReviewedAt != null && QualityReviewedAt != null)
+            {
+                return "AwaitCompletion";
+            }
+
+            return "Unknown";
+        }
+    }
+
+    public string? Creator { get; set; }
+    
+    public string? QualityReviewer => QualityReviewerIdentity?.DisplayName;
+
+    public string? CodeReviewer => CodeReviewerIdentity?.DisplayName;
+    
+    [IgnoreDataMember]
+    private IdentityRefWithVote? QualityReviewerIdentity
+    {
+        get
+        {
+            return Reviewers?.Where(vote => !vote.IsContainer && vote.VotedFor != null && vote.Vote > 0).FirstOrDefault(vote =>
+                vote.VotedFor.First().DisplayName.Contains("Quality") ||
+                vote.VotedFor.First().DisplayName.Contains("QA"));
+        }
+    }
+    
+    [IgnoreDataMember]
+    private IdentityRefWithVote? CodeReviewerIdentity
+    {
+        get
+        {
+            return Reviewers?.Where(vote => !vote.IsContainer && vote.Vote > 0 && !vote.UniqueName.Contains("kje@"))
+                .FirstOrDefault(vote =>
+                    vote.VotedFor == null || (vote.VotedFor != null &&
+                                              !vote.VotedFor.First().DisplayName.Contains("Quality") &&
+                                              !vote.VotedFor.First().DisplayName.Contains("QA")));
+        }
+    }
+    
+    [IgnoreDataMember]
+    public IdentityRefWithVote[]? Reviewers { get; set; }
+
+    public DateTime? QualityReviewedAt { get; set; }
+    
+    public DateTime? CodeReviewedAt { get; set; }
+}
